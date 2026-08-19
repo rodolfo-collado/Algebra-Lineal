d77111b8d23f3a30cc284e68915c3cbc6726adf6
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-08-18 21:51:23 -0600

    feat: add interactive matrix menu using python

---
+++ Semana_1/Tarea2.py
@@ -0,0 +1,70 @@
+lista = [[12, 34, 11], [45, 60, 11], [22, 51, 81]]
+
+
+def pedir_indices():
+    while True:
+        try:
+            fila = int(input("\nIngrese el índice de la fila: "))
+            if fila <= 0 or fila > len(lista):
+                print("Error: fila fuera de rango. Intenta de nuevo")
+            else: break
+
+        except (ValueError or TypeError):
+            print("Error: Ingrese un numero valido.\n")
+            continue
+
+    while True:
+        try:
+            columna = int(input("Ingrese el índice de la columna: "))
+            if columna <= 0 or columna > len(lista[0]):
+                print("Error: columna fuera de rango. Intente de nuevo.\n")
+            else: break
+
+        except (ValueError or TypeError):
+            print("\nError: Ingrese un numero valido.")
+    return fila, columna
+
+
+
+def mostrar_matriz(lista):
+    print("\n--- Matriz Completa ---")
+    for i in lista:
+        print(*i)
+
+
+while True:
+
+    try:
+        print("""
+        \n=== PRÁCTICA DE MATRICES ===
+        
+    --- opciones (índices empiezan en 1) ---
+    
+    1. Modificar elemento
+    2. Consultar elemento
+    3. Ver matriz completa
+    4. Salir
+    """)
+        opcion = input("==> ").strip().lower()
+
+        match opcion:
+            case "1":
+                fila, columna = pedir_indices()
+                numero = int(input("Ingresa el nuevo número: "))
+                lista[fila - 1][columna - 1] = numero
+                print("\nElemento modificado correctamente")
+            case "2":
+                fila, columna = pedir_indices()
+                numero = lista[fila - 1][columna - 1]
+                print(f"\nEl elemento en la posición [{fila},{columna}] = {numero}")
+            case "3":
+                mostrar_matriz(lista)
+            case "4":
+                break
+            case _:
+                print("\nError: Selecciona una opcion válida")
+
+
+    except TypeError:
+        print("\nError: Input Inválido. Intente de nuevo.")
+        continue
